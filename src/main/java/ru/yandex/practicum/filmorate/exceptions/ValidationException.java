ec0e8decd54426532b844e10872097e02ef175d1
Dmitriy <dimka.dyachihin@yandex.ru>
2023-10-03 22:34:44 +0300

    refactor: Убрать ненужный импорт.

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.io.IOException;
-
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class ValidationException extends RuntimeException {
     public ValidationException() {
